cee269df06bd931c08d37c64aa6aa76521e32a1f
brodrickbaker <brodrickrbaker@gmail.com>
2024-07-28 15:39:11 -0400

    get all bookings of current user

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { Spot, Review, ReviewImage, User, SpotImage } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const spot = require('../../db/models/spot');
+
+// get all bookings from current user
+router.get('/current', requireAuth, async (req, res) => {
+    const { user } = req
+    let bookings = await user.getBookings(
+        {
+            include: 
+            {
+                model: Spot,
+                include: {
+                    model: SpotImage
+                }
+            }
+        })
+
+    bookings = bookings.map(booking => {
+        const spot = booking.Spot;
+        bookingPayload = {
+            id: booking.id,
+            spotId: booking.spotId,
+            Spot: {
+                id: spot.id,
+                ownerId: spot.ownerId,
+                address: spot.address,
+                city: spot.city,
+                state: spot.state,
+                country: spot.country,
+                lat: spot.lat,
+                lng: spot.lng,
+                name: spot.name,
+                price: spot.price,
+                previewImage: spot.SpotImages[0].url
+            },
+            userId: booking.userId,
+            startDate: booking.startDate,
+            endDate: booking.endDate,
+            createdAt: booking.createdAt,
+            updatedAt: booking.updatedAt
+        }
+        return bookingPayload
+    })
+    return res.json({'Bookings': bookings})
+})
+
+module.exports = router;
