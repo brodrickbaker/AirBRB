b58cd484e0a9d45981d38c452cfc4f7e962ffbd3
brodrickbaker <brodrickrbaker@gmail.com>
2024-09-03 19:03:22 -0400

    updated seeder dates

--- backend/db/seeders/20240724023029-test-bookings.js
+++ backend/db/seeders/20240724023029-test-bookings.js
@@ -11,26 +11,26 @@ module.exports = {
       {
         spotId: 1,
         userId: 2,
-        startDate: '2024-08-09',
-        endDate: '2024-08-11'
+        startDate: '2025-08-09',
+        endDate: '2025-08-11'
       },
       {
         spotId: 2,
         userId: 1,
-        startDate: '2024-10-09',
-        endDate: '2024-10-11'
+        startDate: '2025-10-09',
+        endDate: '2025-10-11'
       },
       {
         spotId: 3,
         userId: 3,
-        startDate: '2024-11-09',
-        endDate: '2024-12-11'
+        startDate: '2025-11-09',
+        endDate: '2025-12-11'
       },
       {
         spotId: 4,
         userId: 1,
-        startDate: '2024-10-09',
-        endDate: '2024-10-11'
+        startDate: '2025-10-09',
+        endDate: '2025-10-11'
       }
     ], {validate: true})
   },
