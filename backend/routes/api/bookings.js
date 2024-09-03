4f95de01d2a6910465eeeb7fd555d6e96779723b
brodrickbaker <brodrickrbaker@gmail.com>
2024-09-03 18:27:29 -0400

    .

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const e = require('express');
 const { Spot, SpotImage, Booking } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 const { isBooked, preview, notFound, notAuthorized } = require('../../utils/checks');
