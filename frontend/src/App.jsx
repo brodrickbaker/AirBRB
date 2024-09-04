8f52f871547332c957d431787b9165b23997af89
brodrickbaker <brodrickrbaker@gmail.com>
2024-09-04 18:33:51 -0400

    created user session and login form page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,31 @@
+import { createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
+
+const Layout = () => {
+  return (
+    <Outlet/>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout/>,
+    children: 
+    [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>
+      },
+      {
+        path: '/login',
+        element: <LoginFormPage />
+      }
+    ]
+  }
+]);
+
 function App() {
-  return <h1> Hello from App </h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,3 @@
+import LoginFormPage from "./LoginFromPage";
+
+export default LoginFormPage;
